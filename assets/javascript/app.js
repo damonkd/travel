c3851fcf3e6ccc9ff15b5452e31e4295b84a358c
damonkd <47319137+damonkd@users.noreply.github.com>
2019-04-03 19:33:02 -0700

    Add files via upload

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -12,14 +12,12 @@ console.log("hello world");
 
 //GET "https://api.travelpayouts.com/v1/prices/calendar?depart_date=2016-11&origin=MOW&destination=BCN&calendar_type=departure_date&token=a63a34e7f0fbe1a88d351460092f8aa3";
 
-$.get("https://api.travelpayouts.com/v2/prices/latest?currency=usd&period_type=year&page=1&limit=30&show_to_affiliates=true&sorting=price&trip_class=0&token=a63a34e7f0fbe1a88d351460092f8aa3", function(data, status){
+$.get("https://cors-anywhere.herokuapp.com/https://api.travelpayouts.com/v2/prices/latest?currency=usd&period_type=year&page=1&limit=30&show_to_affiliates=true&sorting=price&trip_class=0&token=a63a34e7f0fbe1a88d351460092f8aa3", function(data, status){
     console.log(data);
   });
 
 
 // Create a request variable and assign a new XMLHttpRequest object to it.
-
-
 
   
 
